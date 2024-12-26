b643e54dc7e3dc14dd905ade566696d00b982e74
muneerzain992 <muneerzain992@gmail.com>
2024-12-26 08:23:03 +0500

    Preloader update

---
+++ client/src/components/Preloader.jsx
@@ -0,0 +1,14 @@
+// src/components/Preloader.jsx
+import React from "react";
+import "./Preloader.css";
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Preloader;
